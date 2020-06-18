fe26e51b575adfbde06132a499c0498ac85fd602
katz00 <>
2020-06-18 15:12:16 +0900

    ユーザー登録のリクエストスペック

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "ユーザー登録", type: :request do
   before do
     get signup_path
   end
@@ -8,5 +8,26 @@ RSpec.describe "Users", type: :request do
   it "正常なレスポンスを返すこと" do
     expect(response).to be_success
     expect(response).to have_http_status "200"
+  end
+
+  it "有効なユーザーで登録" do
+    expect {
+      post users_path, params:{ user:{ name: "Example User",
+                                      email: "example@sample.com",
+                                      password: "password",
+                                      password_confirmation: "password"}}
+      }.to change(User, :count).by(1)
+      redirect_to @user
+      follow_redirect!
+      expect(response).to render_template 'users/show'
+  end
+
+  it "無効なユーザーで登録" do
+    expect {
+      post users_path, params:{ user:{ name: "",
+                                      email: "example@sample.com",
+                                      password: "password",
+                                      password_confirmation: "pass"}}
+      }.not_to change(User, :count)
   end
 end
