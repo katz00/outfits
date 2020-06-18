aac83c0237e2e9624a78f2421e8384100697cbf2
katz00 <>
2020-06-18 16:31:35 +0900

    ログインのリクエストスペックとログインページのビューの実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
 
-  get '/login', to: 'serssions#new'
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 end
