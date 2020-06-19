db01964bd7f5dbcca38c6fad8bf6a81cebcbbac0
katz00 <>
2020-06-19 17:31:25 +0900

    タイポckeckboxのラベルにシンボルの:が抜けてた

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,7 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
   # ユーザーを永続ログインさせる
   def remember_me(user)
     user.remember
@@ -21,18 +22,17 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        log_in(user)
+        log_in user
         @current_user = user
       end
     end
   end
+
   # 永続ログインの解除
   def forget(user)
     user.forget
