9a3ae2575549318edede3936ed4eebf3b7e9d2d1
katz00 <>
2020-06-19 16:46:22 +0900

    クッキーにremember_tokenを保存するメソッドを追加、current_userメソッドはsessionかcokkiesに情報が残っているパターンも考慮

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
-
   def logged_in?
     !current_user.nil?
   end
@@ -17,5 +11,25 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  # ユーザーを永続ログインさせる
+  def remember_me(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  # クッキーが持つ記憶トークンに対応するユーザーを返す
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in(user)
+        @current_user = user
+      end
+    end
   end
 end
